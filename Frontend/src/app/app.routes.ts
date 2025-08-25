fa8b111a3e1dca457176594a7c9de3a927570a1a
Carbonell-Castillo <brucecastillo1204@gmail.com>
2025-08-24 18:29:47 -0600

    feat: add home, multas, parqueo, and vehiculos pages with respective components, styles, and templates
    - Created Home component with basic structure.
    - Added Multas component including
    - Implemented Parqueo component with detailed layout for car and motorcycle parking.
    - Developed Vehiculos component to display vehicle cards.
    - Included styles for all new components to enhance UI.
    - Added test specifications for all components to ensure functionality.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,12 +3,35 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./pages/login').then(m => m.LoginComponent), // tu login
+    loadComponent: () => import('./pages/login').then(m => m.LoginComponent),
   },
   {
-    path: 'dashboard',
+    path: 'app', // layout con menú fijo
     loadComponent: () => import('./dashboard/dashboard').then(m => m.DashboardComponent),
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+      },
+      {
+        path: 'parqueo',
+        loadComponent: () => import('./pages/parqueo/parqueo').then(m => m.Parqueo),
+      },
+      {
+        path: 'vehiculos',
+        loadComponent: () => import('./pages/vehiculos/vehiculos').then(m => m.Vehiculos),
+      },
+      {
+        path: 'multas',
+        loadComponent: () => import('./pages/multas/multas').then(m => m.Multas),
+      },
+      {
+        path: 'agregarVehiculos',
+        loadComponent: () => import('./pages/agregar-vehiculos/agregar-vehiculos').then(m => m.AgregarVehiculos),
+      },
+    ],
   },
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: '**', redirectTo: 'login' },
+  { path: '', pathMatch: 'full', redirectTo: 'app' },
+  { path: '**', redirectTo: 'app' },
 ];
---
+++ Frontend/src/app/pages/agregar-vehiculos/agregar-vehiculos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-vehiculos',
+  imports: [],
+  templateUrl: './agregar-vehiculos.html',
+  styleUrl: './agregar-vehiculos.css'
+})
+export class AgregarVehiculos {
+
+}
