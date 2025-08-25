36dba64ed4dfcdaba11c928d942fa7f4a8bf983c
LuisPerez99 <3008387860101@ingenieria.usac.edu.gt>
2025-08-25 15:21:52 -0600

    feat(parqueo): Add asignarParqueoUsuario route to API

--- backend/src/modules/parqueo/routes.js
+++ backend/src/modules/parqueo/routes.js
@@ -29,4 +29,17 @@ router.post('/asignar', (req, res) => {
         });
 });
 
+router.post('/asignar-usuario', (req, res) => {
+   const body = req.body;
+   
+   controller.asignarParqueoUsuario(body)
+       .then((result) => {
+           genericResponse.success(req, res, result, 200);
+       })
+       .catch((error) => {
+           console.log(error);
+           genericResponse.error(req, res, error, 500);
+       });
+});
+
 module.exports = router;
