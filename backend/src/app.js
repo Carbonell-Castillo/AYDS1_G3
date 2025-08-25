b3f674a1a68fa8ef1bb56f4a22f24e71d15631ab
LuisPerez99 <3008387860101@ingenieria.usac.edu.gt>
2025-08-24 23:42:26 -0600

    style(app): Refactor code formatting for consistency

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const config = require ('./config');
+const express = require("express");
+const config = require("./config");
 
-const users = require ('./modules/users/routes.js');
+const users = require("./modules/users/routes.js");
+const parqueo = require("./modules/parqueo/routes.js");
 
 const app = express();
 
-app.set('port',config.appConfig.port);
+app.set("port", config.appConfig.port);
+app.use(express.json());
 
-app.use('/api/users',users);
-
-
+app.use("/api/users", users);
+app.use("/api/parqueo", parqueo);
 
 module.exports = app;
-
